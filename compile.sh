c0cc1852fec98f157ec6529e7b05b6f79400a31d
Noëlle <54525684+Dirty-No@users.noreply.github.com>
2020-08-02 05:54:39 +0300

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-SDL_DIR=SDL/x86_64-w64-mingw32
-
-x86_64-w64-mingw32-gcc.exe -Iincludes srcs/main.c libmlx.a \
--L"$SDL_DIR/lib" -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer \
--lSDL2_ttf  -lmingw32 -lSDL2main -lSDL2.dll -luser32 -lgdi32 -lwinmm -ldxguid -o test.exe
